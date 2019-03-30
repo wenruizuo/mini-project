1ec75bfd7c9f21f0127dc313e37ae748a78ebf9b
wenruizuo <49080819+wenruizuo@users.noreply.github.com>
2019-03-30 15:25:08 +0000

    Update app.py

--- week10/project/app.py
+++ week10/project/app.py
@@ -13,10 +13,11 @@ url_template = 'https://api.betterdoctor.com/2016-03-01/doctors?first_name={name
 
 
 
-@app.route('/External/<name>',  methods=['GET'])
+@app.route('/External/<name>',  methods=['GET']) #/External/<name> is new address to get all involved information from external web#
 def doctorchart(name):
     url = url_template.format(name=name,api_key=userkey)
-
+#complete interaction with external REST services,#
+#Use of on an external Cloud database for persisting information#
 
     # header = {"User-agent":"crul/7.43.0", "Accept": "application/json", "user_key": userkey}
     resp = requests.get(url)#,header=header)
@@ -31,10 +32,10 @@ def doctorchart(name):
 
 @app.route('/Cassandra/<name>')
 def profile(name):
-    rows = session.execute( """Select * From doctor.stats where name = '{}'""".format(name))
+    rows = session.execute( """Select * From doctor.stats where name = '{}'""".format(name)) #select sepecific doctor's database#
     for row in rows:
-        return('<h1>{} average charge amt =  {} !</h1>'.format(name,doctor.average_charge_amt))
-    return('<h1>That name does not exist!</h1>')
+        return('<h1>{} average charge amt =  {} !</h1>'.format(name,doctor.average_charge_amt))     #Output {} average charge amt =  {} !#
+    return('<h1>That name does not exist!</h1>') #If name is not in data list,web page will inform#
 
 
 if __name__=="__main__":
